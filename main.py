a5351fed580c96713710b87484f8f29d81a76a00
Hoppli <alex.kovak@gmail.com>
2026-02-15 15:31:49 -0500

    C. Added FPS Throttle

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    FPS_clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
@@ -16,6 +18,7 @@ def main():
         
         screen.fill("black")
         pygame.display.flip()
+        dt = FPS_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
